f2d306a712cae973dfe8d8710a2a72bd7457dc4f
CharliGithub <31701566+CharliGithub@users.noreply.github.com>
2019-10-06 12:47:31 +0100

    Update whatsmatter.py

--- whatsmatter.py
+++ whatsmatter.py
@@ -11,27 +11,27 @@ import json
 import emojis
 
 def job():    
-    global a,n    
-    data = requests.get('http://guidos.000webhostapp.com/dqfqfezqfezgrt.php')
+    global a,n,url    
+    data = requests.get(url)
     try:
         a = json.loads(data.content)    
-    except JSONDecodeError as e:
+    except ValueError as e:    
         print("Can't reach hackerlab results")
         sys.exit()
     n.set_urgency(notify2.URGENCY_NORMAL)
     n.set_timeout(10000)
-    n.update(emojis.encode(':bell: #Hackerlab2019: Les Finalistes'), emojis.encode('Numéro '+str(a['users'][-1]['id'])+': '+a['users'][-1]['username']+' est qualifié pour le #Hackerlab2019 :smile:'))        
+    n.update(emojis.encode(':bell: #Hackerlab2019: Les Finalistes'), emojis.encode('Numéro '+str(a['users'][-1]['id'])+': '+str(a['users'][-1]['username']).capitalize()+' est qualifié pour le #Hackerlab2019 :smile:'))        
 
 if __name__ == '__main__':    
-    first_content,a = "",""
-    data = requests.get('http://guidos.000webhostapp.com/dqfqfezqfezgrt.php')
+    first_content,a,url = "","",'http://guidos.000webhostapp.com/dqfqfezqfezgrt.php'
+    data = requests.get(url)
     if data.status_code != 200: 
         print("Can't reach hackerlab results")
         sys.exit()
     else:
         try:
             first_content = json.loads(data.content)
-        except JSONDecodeError as e:
+        except ValueError as e:
             print("Can't reach hackerlab results")
             sys.exit()        
         notify2.init('Hackerlab2019 Qualifications news')
@@ -43,3 +43,4 @@ if __name__ == '__main__':
             n.show()
             first_content = a        
             time.sleep(1)
+
